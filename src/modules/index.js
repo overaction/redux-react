10e5f2420204af0a4fdc914fc8f1b6a4171e6224
overaction <kmc04@naver.com>
2020-03-20 14:46:43 +0000

    스토어 생성

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,5 @@
+// createStore 함수에서 리듀서 함수는 하나만 사용할 수 있기 때문에
+// combineReducers를 사용해서 하나로 합쳐줌
 import {combineReducers} from 'redux';
 import counter from './counter';
 import todos from './todos';
