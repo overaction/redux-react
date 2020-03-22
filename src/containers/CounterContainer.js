9ab77a52ae5dbd646406a3842b3981b2b0055d6c
overaction <kmc04@naver.com>
2020-03-22 14:30:16 +0000

    hooks를 사용해서 Counter 컨테이너 컴포넌트 작성

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,13 +1,21 @@
 // 리덕스 스토어와 연동된 컨테이너인 컨테이너 컴포넌트 생성
-import React from 'react';
-import {connect} from 'react-redux';
+import React, { useCallback } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
-    return <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+//useDispatch는 useCallback과 함께 사용하는 습관을 들이자
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()),[dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()),[dispatch]);
+    return (
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+    )
 };
 
+/*
 // 컨테이너 컴포넌트를 리덕스와 연동하기 위해서는 connect 함수를 사용해야한다
 export default connect(
     (sta) => ({
@@ -18,3 +26,6 @@ export default connect(
         decrease
     }
 )(CounterContainer)
+*/
+
+export default CounterContainer;
